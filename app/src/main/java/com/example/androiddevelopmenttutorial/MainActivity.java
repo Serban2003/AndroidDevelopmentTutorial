4a0d6a2608165caaad5aa3a403a8b7957bb32c91
Iustinian Serban <iustinianserban@gmail.com>
2022-07-24 21:00:58 +0300

    Create Simple Registration Form

--- app/src/main/java/com/example/androiddevelopmenttutorial/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopmenttutorial/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.androiddevelopmenttutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onButtonClick(View view){
+        EditText editTextFirstName = findViewById(R.id.editTextFirstName);
+        EditText editTextLastName = findViewById(R.id.editTextLastName);
+        EditText editTextEmail = findViewById(R.id.editTextEmailAddress);
+
+        TextView messageFirstName = findViewById(R.id.textViewFirstName);
+        TextView messageLastName = findViewById(R.id.textViewLastName);
+        TextView messageEmail = findViewById(R.id.textViewEmail);
+
+        messageFirstName.setText("First Name: " + editTextFirstName.getText().toString());
+        messageLastName.setText("Last Name: " + editTextLastName.getText().toString());
+        messageEmail.setText("Email: " + editTextEmail.getText().toString());
     }
 }
